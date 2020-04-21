b9a5fe9505dcc7fd1419a4e5656c4f556351c2ed
Ivan <ivandaixivwork@gmail.com>
2020-04-21 15:36:33 -0700

    Adds the ImageList component

---
+++ src/components/ImageList/ImageList.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ImageList = () => {
+  return <h1>This is an imagelist</h1>;
+};
+export default ImageList;
---
+++ src/components/ImageList/ImageListContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ImageList from "./ImageList";
+
+export default class ImageListContainer extends Component {
+  render() {
+    return <ImageList></ImageList>;
+  }
+}
