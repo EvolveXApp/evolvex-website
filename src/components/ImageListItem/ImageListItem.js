6ddbd6cfbbf47534f0a4eaa910702974c9465f23
Ivan <ivandaixivwork@gmail.com>
2020-04-21 17:13:00 -0700

    Adds mapping through the dummy data on imagelist

--- src/components/ImageListItem/ImageListItem.js
+++ src/components/ImageListItem/ImageListItem.js
@@ -1,7 +1,22 @@
 import React from "react";
-
-const ImageListItem = () => {
-  return <p>Image List Item</p>;
+const ImageListItem = (props) => {
+  const { data } = props;
+  console.log("Data", data);
+  return (
+    <div>
+      {data.map((item) => (
+        <div>
+          <img
+            src={item.image}
+            alt={item.title}
+            title={item.title}
+            style={{ width: 100, height: 100 }}
+          />
+          <p>Hello World</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ImageListItem;
