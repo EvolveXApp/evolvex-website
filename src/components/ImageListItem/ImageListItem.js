a02ff372b50f38ceaead5c462d05762f34d04c41
Ivan <ivandaixivwork@gmail.com>
2020-04-22 18:12:28 -0700

    Adds alternating between theme colours

--- src/components/ImageListItem/ImageListItem.js
+++ src/components/ImageListItem/ImageListItem.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 
 const ImageListItem = (props) => {
   const { data, classes } = props;
   console.log("Data", data);
+  const backgroundColours = ["#3671B7", "#ECB71F", "#DE1C54"];
+  let colourValue = 0;
   return (
     <div className={classes.container}>
       {data.map((item) => (
         <div className={classes.item} key={item.position}>
-          <img
-            src={item.image}
-            alt={item.title}
-            title={item.title}
+          <div
+            style={{ backgroundColor: backgroundColours[colourValue] }}
             className={
-              item.position % 2 === 0 ? classes.flipImage : classes.image
+              item.position % 2 === 0
+                ? classes.flipImage
+                : classes.imageContainer
             }
-          />
-          <div className={item.id % 2 === 0 && classes.flipText}>
-            <p className={classes.title}>{item.title}</p>
+          >
+            <img src={item.image} alt={item.title} title={item.title} />
+          </div>
+          <div
+            className={
+              (classes.textContainer, item.id % 2 === 0 && classes.flipText)
+            }
+          >
+            <div className={classes.titleContainer}>
+              <p className={classes.title}>{item.title}</p>
+            </div>
             <p className={classes.description}>{item.description}</p>
           </div>
+          {colourValue === 2 ? (colourValue = 0) : colourValue++}
         </div>
       ))}
     </div>
--- src/components/ImageListItem/styles.js
+++ src/components/ImageListItem/styles.js
@@ -3,12 +3,30 @@ import { createStyles } from "@material-ui/core/styles";
 const styles = createStyles({
   container: {
     background: "#111111",
+    padding: "50px 0",
   },
   item: {
     display: "flex",
+    justifyContent: "space-between",
     flexDirection: "row",
   },
+  imageContainer: {
+    borderRadius: 5,
+    position: "relative",
+    margin: "50px 0",
+    padding: "0px 35px 0px 100px",
+    "& img": {
+      transform: `translate(00px, -50px)`,
+    },
+  },
   flipImage: {
+    borderRadius: 5,
+    position: "relative",
+    margin: "50px 0",
+    padding: "0px 100px 0px 35px",
+    "& img": {
+      transform: `translate(00px, -50px)`,
+    },
     display: "grid",
     order: 2,
   },
@@ -16,11 +34,19 @@ const styles = createStyles({
     display: "grid",
     order: 1,
   },
+  titleContainer: {
+    borderBottom: "2px solid #ECB71F",
+    width: 300,
+  },
   title: {
+    width: "90%",
     color: "#f1f1f1",
+    fontSize: 24,
   },
   description: {
+    width: 500,
     color: "#939393",
+    fontSize: 18,
   },
 });
 
