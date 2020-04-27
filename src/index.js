f47b7d6520b9102634c4084abd5911920ae208a4
Yen Yin Hing <yenyinf287@gmail.com>
2020-04-26 17:09:13 -0700

    add gilroy font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "./assets/fonts/Gilroy-Regular.otf";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
