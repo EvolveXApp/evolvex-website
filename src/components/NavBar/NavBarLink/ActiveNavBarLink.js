e96a5ffc9b8e45e8c8aa32c6a4a369f6fb9eb7d3
Yen Yin Hing <yenyinf287@gmail.com>
2020-04-30 14:46:33 -0700

    add props

--- src/components/NavBar/NavBarLink/ActiveNavBarLink.js
+++ src/components/NavBar/NavBarLink/ActiveNavBarLink.js
@@ -2,13 +2,14 @@ import React from "react";
 import { NavBarLink } from "./NavBarLink";
 import styles from "./NavBarLink.module.scss";
 
-export function ActiveNavBarLink({ to, title }) {
+export function ActiveNavBarLink({ to, title, ...props }) {
   return (
     <NavBarLink
       className={`${styles.navLink} ${styles.activeNavLink}`}
       to={to}
       title={title}
       activeStyle={{ borderColor: "#EEB91F" }}
+      {...props}
     />
   );
 }
