354f1f3248f2900acd237f46c6c456427934495e
Ivan <ivandaixivwork@gmail.com>
2020-05-01 18:27:17 -0700

    Adds testimonials component directory

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Testimonials = () => {
+  return <p>Testimonials Page</p>;
+};
+
+export default Testimonials;
---
+++ src/components/Testimonials/TestimonialsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Testimonials from "./Testimonials";
+
+class TestimonialsContainer extends Component {
+  render() {
+    return <Testimonials></Testimonials>;
+  }
+}
+
+export default TestimonialsContainer;
