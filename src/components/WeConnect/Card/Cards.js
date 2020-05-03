f0eb085f01f96e4800abef10f3c33493472ff192
Yen Yin Hing <yenyinf287@gmail.com>
2020-05-03 15:06:06 -0700

    implement and style WeConnect section- create carditem-add weconnect component to home

---
+++ src/components/WeConnect/Card/Cards.js
@@ -0,0 +1,27 @@
+import business from "../../../assets/icons/business-icon.png";
+import creatives from "../../../assets/icons/creatives-icon.png";
+import investors from "../../../assets/icons/investor-icon.png";
+
+export const Cards = [
+  {
+    id: 1,
+    image: creatives,
+    name: "Creatives",
+    content:
+      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+  },
+  {
+    id: 2,
+    image: business,
+    name: "Creatives",
+    content:
+      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+  },
+  {
+    id: 3,
+    image: investors,
+    name: "Creatives",
+    content:
+      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+  },
+];
