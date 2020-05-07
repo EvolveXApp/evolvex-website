1e2b4e458c1234851d6fb1fab1f44a6812f68b3e
Yen Yin Hing <yenyinf287@gmail.com>
2020-05-06 19:38:58 -0700

    add download icon feature

--- src/components/ResourceType/ResourceCard/ResourceCardItem/ResourceCardItem.js
+++ src/components/ResourceType/ResourceCard/ResourceCardItem/ResourceCardItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./ResourceCardItem.module.scss";
+import downloadIcon from "../../../../assets/icons/download-icon.png";
 
 export default function ResourceCardItem({ title, link }) {
   return (
     <div className={styles.container}>
       <div className={styles.title}>{title}</div>
       <div className={styles.link}>{link}</div>
+      <img src={downloadIcon} className={styles.icon} />
     </div>
   );
 }
