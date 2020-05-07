be46d662c543988c3168b54f76ac866bccb9efb4
Yen Yin Hing <yenyinf287@gmail.com>
2020-05-06 19:39:43 -0700

    fix typo

--- src/components/WeConnect/Card/Cards.js
+++ src/components/WeConnect/Card/Cards.js
@@ -8,20 +8,20 @@ export const Cards = [
     image: creatives,
     name: "Creatives",
     content:
-      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams and agile workflows emulate that of an agency.",
   },
   {
     id: 2,
     image: business,
-    name: "Creatives",
+    name: "Business",
     content:
-      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+      "We bring value to underfunded businesses by providing them with affordable work from our interns and high level business strategy from our experienced team of profesionals.",
   },
   {
     id: 3,
     image: investors,
-    name: "Creatives",
+    name: "Investors",
     content:
-      "We give digital creatives access to an abundance of different projects to help expand their portfolios wtih real brands. Our dynamic teams an agile workflows emulate that of an agency",
+      "When we see promise in an up and coming business, we often help them with fundraising to ensure success for the business after the duration of their involvement with Evolve X",
   },
 ];
