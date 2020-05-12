8e8c17464351796d6066f8f28f4af450a9709722
Jasmine Kaur <jasmine.sodhi3@gmail.com>
2020-05-12 00:36:05 -0700

    Solves some conflict error while committing changes

---
+++ src/components/NewProject/SocialContact/SocialContactData.js
@@ -0,0 +1,31 @@
+import Facebook from "../../../assets/icons/facebook.png";
+import Google from "../../../assets/icons/Google.png"
+import IG from "../../../assets/icons/IG.png";
+import Dribble from "../../../assets/icons/Dribbble.png"
+import Vimeo from "../../../assets/icons/Vimeo.png";
+
+
+export const SocialLinks = [
+{
+    id:1,
+    imgSrc:Vimeo
+},
+{
+    id:2,
+    imgSrc:Facebook
+},
+{
+    id:3,
+    imgSrc:IG
+},
+{
+    id:4,
+    imgSrc:Google
+},
+{
+    id:5,
+    imgSrc:Dribble
+}
+
+
+];
--- src/pages/Resources/Resources.js
+++ src/pages/Resources/Resources.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ResourceType from "../../components/ResourceType/";
 import Header from "../../components/Header";
+import "./style.scss";
 
 class Resources extends Component {
   render() {
@@ -9,7 +10,7 @@ class Resources extends Component {
         <Header heading="We're Here to Help You Learn"  text="The evolveˣ organization is developed to serve entrepreneurs, 
         designers, developers and media professionals globally. We develop 
         key market awareness for young and seasoned professionals across a 
-        wide spectrum of industries."  imageSrc={"photo-1574315042617-f84e1847848a.jpeg"}/>
+        wide spectrum of industries."  imageSrc={"Screen Shot 2020-04-01 at 2.18 1.png"} imageDimensions="header-image-resources"/>
         <ResourceType />{" "}
 
       </div>
