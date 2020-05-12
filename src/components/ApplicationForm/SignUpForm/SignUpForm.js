8d2f49e065c2e77d35f683c0d46a0c82a845a197
Jasmine Kaur <jasmine.sodhi3@gmail.com>
2020-05-12 00:03:42 -0700

    Adds Resources Form when Application Form is submitted

--- src/components/ApplicationForm/SignUpForm/SignUpForm.js
+++ src/components/ApplicationForm/SignUpForm/SignUpForm.js
@@ -1,43 +1,43 @@
 import React,{Component} from "react";
 import "./form.scss";
 import ViewResourcesForm from "../ViewResourcesForm";
-
+import Form from "./Form";
 
 class SignUpForm extends Component
 {
-    state = { showing: true };
+    constructor(props)
+    {
+        super(props);
+        this.isFormNotSubmitted = this.handleFormSubmission.bind(this);
+        this.state = {isFormNotSubmitted: true};
+    }
+
+    handleFormSubmission()
+    {
+        this.setState({isFormNotSubmitted: false});
+    }
+
     render(){
-        const { showing } = this.state
-        return(
-            <div className="form-wrapper">
-                <form className="form">
-                    <div className="form-container">
-                        <input className="fields" type="text" value="" placeholder="Name" readOnly />
-                        <input className="fields" type="text" value="" placeholder="Portfolio" readOnly />
-                        <input className="fields" type="text" value="" placeholder="Email" readOnly />
+        const isFormNotSubmitted = this.state.isFormNotSubmitted;
+        
+        if(isFormNotSubmitted)
+        {
+            return(
+                <div className="form-wrapper">
                         
-                        <input className="fields" type="text" value="" placeholder="LinkedIn" readOnly />
-                        <input className="fields" type="text" value="" placeholder="Phone" readOnly />
-                        <input className ="buttons" type="button" value="Resume / CV" />
-                        <input className="fields" type="text" value="" placeholder="Profession" readOnly />
+                    <Form />
+                    <button className="submitButton" onClick ={() => this.handleFormSubmission()} > Submit</button>
                         
-                        <input className="buttons" type="button" value="Cover Letter" />
-
-                        
-                    </div>
-                
-                    <input className="submitButton" type="submit" value="Submit" onClick = {() => this.setState({showing: !showing})} />
-                    {
-                        showing ? <ViewResourcesForm /> : null
-
-                    }
-                    
-                    
-                </form>
-            
-            </div>    
-        );
+                </div>
+            );
+        }
+        else
+        {
+            return(
+                <ViewResourcesForm />
+            );
+        }
     }
 }
-
 export default SignUpForm;
+
