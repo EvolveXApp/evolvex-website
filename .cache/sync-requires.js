e399c98360488c1adcc3c5543826a185ddfdd361
Yen Yin Hing <yenyinf287@gmail.com>
2020-06-11 19:56:02 -0700

    [+] gatsby feature

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,19 +5,20 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\404.js"))),
-  "component---src-pages-contact-contact-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Contact\\Contact.js"))),
-  "component---src-pages-contact-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Contact\\index.js"))),
-  "component---src-pages-home-home-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Home\\Home.js"))),
-  "component---src-pages-home-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Home\\index.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\index.js"))),
-  "component---src-pages-program-overview-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\ProgramOverview\\index.js"))),
-  "component---src-pages-program-overview-program-overview-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\ProgramOverview\\ProgramOverview.js"))),
-  "component---src-pages-resources-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Resources\\index.js"))),
-  "component---src-pages-resources-resources-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Resources\\Resources.js"))),
-  "component---src-pages-team-index-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Team\\index.js"))),
-  "component---src-pages-team-style-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Team\\style.js"))),
-  "component---src-pages-team-team-container-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Team\\TeamContainer.js"))),
-  "component---src-pages-team-team-js": hot(preferDefault(require("C:\\Users\\Jasmine Kaur\\Desktop\\evolvex-website\\src\\pages\\Team\\Team.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/404.js"))),
+  "component---src-pages-contact-contact-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Contact/Contact.js"))),
+  "component---src-pages-contact-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Contact/index.js"))),
+  "component---src-pages-home-home-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Home/Home.js"))),
+  "component---src-pages-home-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Home/index.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/index.js"))),
+  "component---src-pages-program-overview-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/ProgramOverview/index.js"))),
+  "component---src-pages-program-overview-program-overview-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/ProgramOverview/ProgramOverview.js"))),
+  "component---src-pages-resources-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Resources/index.js"))),
+  "component---src-pages-resources-resources-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Resources/Resources.js"))),
+  "component---src-pages-team-index-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Team/index.js"))),
+  "component---src-pages-team-style-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Team/style.js"))),
+  "component---src-pages-team-team-container-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Team/TeamContainer.js"))),
+  "component---src-pages-team-team-js": hot(preferDefault(require("/Users/hingyenyin/Documents/evolvex_projects/evolvex_website/src/pages/Team/Team.js")))
 }
 
