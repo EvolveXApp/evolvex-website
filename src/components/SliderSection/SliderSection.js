dc275f048198dc43b58b8c273f91edc72b365efd
Yen Yin Hing <yenyinf287@gmail.com>
2020-06-25 11:55:12 -0700

    [+] icons [+] style custom arrows

--- src/components/SliderSection/SliderSection.js
+++ src/components/SliderSection/SliderSection.js
@@ -5,6 +5,34 @@ import styles from "./SliderSection.module.scss";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import rightArrow from "../../assets/icons/right_arrow.png";
+import leftArrow from "../../assets/icons/left_arrow.png";
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    >
+      <img src={rightArrow} alt="arrow_left" />
+    </div>
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block" }}
+      onClick={onClick}
+    >
+      <img src={leftArrow} alt="arrow_left" />
+    </div>
+  );
+}
 
 export default function SliderSection() {
   let settings = {
@@ -13,6 +41,8 @@ export default function SliderSection() {
     slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     className: "slides",
 
     responsive: [
