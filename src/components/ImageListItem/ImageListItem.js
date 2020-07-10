ff879bfad4e4f9991caa0a6dcda70ca5a5229efe
Ivan <ivandaixivwork@gmail.com>
2020-07-10 12:38:36 -0700

    Fix the displaying of increasing the colours number pattern

--- src/components/ImageListItem/ImageListItem.js
+++ src/components/ImageListItem/ImageListItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import styles from "./styles";
+import style from "./styles.js";
+import styles from "./styles.module.scss";
 
 const ImageListItem = (props) => {
   const { data, classes } = props;
   const backgroundColours = ["#3671B7", "#ECB71F", "#DE1C54"];
   let colourValue = 0;
+  const handleColour = () => {
+    colourValue === 2 ? (colourValue = 0) : colourValue++;
+  };
   return (
-    <div className={classes.container}>
+    <div class={styles.container}>
       {data.map((item) => (
-        <div className={classes.item} key={item.position}>
+        <div className={styles.item} key={item.position}>
           <div
             style={{ backgroundColor: backgroundColours[colourValue] }}
             className={
@@ -30,11 +34,11 @@ const ImageListItem = (props) => {
             </div>
             <p className={classes.description}>{item.description}</p>
           </div>
-          {colourValue === 2 ? (colourValue = 0) : colourValue++}
+          {handleColour()}
         </div>
       ))}
     </div>
   );
 };
 
-export default withStyles(styles)(ImageListItem);
+export default withStyles(style)(ImageListItem);
