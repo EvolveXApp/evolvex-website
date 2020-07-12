d4e57a75300833767b5fe3e80a8a752485a31868
Yen Yin Hing <yenyinf287@gmail.com>
2020-07-12 16:29:52 -0700

    add formName key prop

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,27 +2,33 @@ import React, { Component } from "react";
 import Header from "../../components/Header";
 import ApplicationForm from "../../components/ApplicationForm";
 import NewProject from "../../components/NewProject";
-import Layout from "../../components/Layout"
+import Layout from "../../components/Layout";
 import "./style.scss";
 
 class Contact extends Component {
   render() {
-    return<Layout>
-    <div className="contact-page">
-        <Header headerText="header-text-contact" heading="Get In Touch"  text="Feel free to send us a message. We pride ourselves in communication. 
+    return (
+      <Layout>
+        <div className="contact-page">
+          <Header
+            headerText="header-text-contact"
+            heading="Get In Touch"
+            text="Feel free to send us a message. We pride ourselves in communication. 
         If you don't hear back from us right away, thats a good thing! That
-         means our program is thriving. Give us a call or text message instead."  imageSrc={"photo-1574315042617-f84e1847848a.jpeg"}
-         imageDimensions = "header-image-contact"/>
-        <NewProject />
-        
-        <ApplicationForm  headerStyle="border-yellow" heading="Your Career Starts Here"/>
-        
+         means our program is thriving. Give us a call or text message instead."
+            imageSrc={"photo-1574315042617-f84e1847848a.jpeg"}
+            imageDimensions="header-image-contact"
+          />
+          <NewProject />
 
-      </div>
+          <ApplicationForm
+            headerStyle="border-yellow"
+            heading="Your Career Starts Here"
+            formName="apply"
+          />
+        </div>
       </Layout>
-      ;
-
-
+    );
   }
 }
 export default Contact;
