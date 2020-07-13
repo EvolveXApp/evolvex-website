ec2087b9ed774c81a73cc1c7625250421572b6fa
Yen Yin Hing <yenyinf287@gmail.com>
2020-07-12 17:30:21 -0700

    [+] "to" attribute

--- src/components/Footer/PageLinks/PageLinks.js
+++ src/components/Footer/PageLinks/PageLinks.js
@@ -6,22 +6,32 @@ export default function PageLinks() {
   return (
     <div className={styles.container}>
       <div className={styles.infoWrapper}>
-        <Link className={styles.mainLink}>Home</Link>
+        <Link to="/" className={styles.mainLink}>
+          Home
+        </Link>
         <Link className={styles.subLink}>Apply</Link>
       </div>
       <div className={styles.infoWrapper}>
-        <Link className={styles.mainLink}>About</Link>
-        <Link className={styles.subLink}>Program</Link>
-        <Link className={styles.subLink}>Team</Link>
+        <div className={styles.mainLink}>About</div>
+        <Link to="/ProgramOverview" className={styles.subLink}>
+          Program
+        </Link>
+        <Link to="/Team" className={styles.subLink}>
+          Team
+        </Link>
       </div>
       <div className={styles.infoWrapper}>
-        <Link className={styles.mainLink}>Resources</Link>
+        <Link to="/Resources" className={styles.mainLink}>
+          Resources
+        </Link>
         <Link className={styles.subLink}>Intern</Link>
         <Link className={styles.subLink}>Branding</Link>
         <Link className={styles.subLink}>Proposals</Link>
       </div>
       <div className={styles.infoWrapper}>
-        <Link className={styles.mainLink}>Contact</Link>
+        <Link to="/Contact" className={styles.mainLink}>
+          Contact
+        </Link>
         <Link className={styles.subLink}>Partnerships</Link>
         <Link className={styles.subLink}>Evolvex</Link>
       </div>
