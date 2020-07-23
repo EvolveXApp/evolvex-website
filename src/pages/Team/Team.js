13377bdcbcf12a9df7d89af20d2e6944eb2e43fc
Ivan <ivandaixivwork@gmail.com>
2020-07-23 14:56:38 -0700

    Add set up for translating from inline JS styling to Sass

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,37 +1,35 @@
 import React from "react";
 import TeamCard from "../../components/TeamCard";
-import { withStyles } from "@material-ui/core/styles";
-import { createStyles } from "@material-ui/core/styles";
-import Layout from "../../components/Layout"
+import Layout from "../../components/Layout";
+import styles from "./styles.module.scss";
 
-const styles = createStyles({
-  teams: {
-    maxWidth: "70vw",
-    margin: "0 auto",
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr",
-  },
-  title: {
-    display: "flex",
-    justifyContent: "center",
-    margin: "50px auto",
-    fontSize: "2rem",
-    borderBottom: "2px solid #3671B7",
-    width: 400,
-  },
-});
+// const styles = createStyles({
+//   teams: {
+//     maxWidth: "70vw",
+//     margin: "0 auto",
+//     display: "grid",
+//     gridTemplateColumns: "1fr 1fr 1fr",
+//   },
+//   title: {
+//     display: "flex",
+//     justifyContent: "center",
+//     margin: "50px auto",
+//     fontSize: "2rem",
+//     borderBottom: "2px solid #3671B7",
+//     width: 400,
+//   },
+// });
 
-const Team = ({ classes, data }) => {
+const Team = ({ data }) => {
   return (
     <Layout>
-      <h2 className={classes.title}>Meet the team</h2>
-      <div className={classes.teams}>
-        {data && data.map((member, index) => (
-          <TeamCard member={member} key={index} />
-        ))}
+      <h2 className={styles.title}>Meet the team</h2>
+      <div className={styles.teams}>
+        {data &&
+          data.map((member, index) => <TeamCard member={member} key={index} />)}
       </div>
     </Layout>
   );
 };
 
-export default withStyles(styles)(Team);
+export default Team;
